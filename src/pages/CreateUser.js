7d91a402da03216a8862efa9e8c66ccf645ad09c
Richard Pentecost <richard.pentecost@madetech.com>
2021-06-01 10:14:13 +0100

    create user form created

--- src/pages/CreateUser.js
+++ src/pages/CreateUser.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import classes from '../style/CreateUser.module.scss';
 
 const CreateUser = () => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  const formSubmit = event => {
+    event.preventDefault();
+    console.log('Form submitted');
+  };
+  
   return (
-    <div>
-      Create User
+    <div classes={classes.createUser}>
+      <form onSubmit={formSubmit}>
+        <Input type='text' ref={nameRef}>Name</Input>
+        <Input type='email' ref={emailRef}>Email</Input>
+        <Input type='password' ref={passwordRef}>Password</Input>
+        <Input type='passowrd' ref={confirmPasswordRef}>Confirm Password</Input>
+        <Button type='submit'>Create User</Button>
+      </form>
     </div>
   )
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,10 +10,8 @@ const Login = ({ history, setToken }) => {
 
   const formSubmit = event => {
     event.preventDefault()
-    console.log(emailRef.current.value);
-    console.log(passwordRef.current.value);
+    
     if (emailRef.current.value === 'Richard' && passwordRef.current.value === 'asdf') {
-      console.log('Here');
       setToken(true);
       history.push('/home');
     }
@@ -22,9 +20,9 @@ const Login = ({ history, setToken }) => {
   return (
     <div className={classes.login}>
       <form onSubmit={formSubmit}>
-        <Input type="text" ref={emailRef}>Email</Input>
-        <Input type="password" ref={passwordRef}>Password</Input>
-        <Button type="submit">Login</Button>
+        <Input type='text' ref={emailRef}>Email</Input>
+        <Input type='password' ref={passwordRef}>Password</Input>
+        <Button type='submit'>Login</Button>
       </form>
     </div>
   );
